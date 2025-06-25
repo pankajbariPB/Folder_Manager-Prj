3baf9dd309756628302e4218ac5be0cf3d9d3cb6
pankajbariPB <pankajbari13official@gmail.com>
2025-06-25 10:55:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+Project Title: Folder Manager
+==================================================================================================
+
+Author:
+Pankaj Bari
+
+Version:
+1
+
+Description:
+Folder Manager is a simple yet powerful Python script that helps you organize your files effortlessly.
+ It automatically scans a target directory and sorts all files into subfolders based on their file extensions 
+ (e.g., .pdf files go into a pdf folder). This keeps your folders neat, tidy,
+and easy to navigate—perfect for managing downloads, documents, or any cluttered directory.
+✨ Features
+1. Automatic File Organization: Sorts files into subfolders by extension.
+2. No Setup Required: Just specify your folder path and run the script.
+3. Handles All File Types: Supports files with or without extensions.
+4. Safe & Efficient: Skips directories and creates folders only as needed.
+5. Cross-Platform: Works on Windows, macOS, and Linux.
+
+🚀 How It Works
+Specify the folder you want to organize.
+
+Run the script.
+
+All files are moved into subfolders named after their extensions (e.g., jpg, pdf, no_extension).
+
+"""
