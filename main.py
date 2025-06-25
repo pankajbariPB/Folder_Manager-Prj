2d621257847a77c1b5307c6efd810901953f805b
pankajbariPB <pankajbari13official@gmail.com>
2025-06-25 10:59:46 +0530

    first version

--- main.py
+++ main.py
@@ -28,3 +28,24 @@ Run the script.
 All files are moved into subfolders named after their extensions (e.g., jpg, pdf, no_extension).
 
 """
+# C:\Users\panka\Downloads\test.pdf
+# C:\Users\panka\Downloads\pdf\text.pdf
+import os,shutil
+
+folder_path= r"C:\Users\panka\Downloads" 
+
+try:
+    for item in os.listdir(folder_path):
+        item_path=os.path.join(folder_path,item)
+        if os.path.isfile(item_path):
+            _ ,extension=os.path.splitext(item)
+            ext_folder=os.path.join(folder_path,extension[1:].lower() or "no_extension")
+
+            os.makedirs(ext_folder,exist_ok=True)
+            shutil.move(item_path,os.path.join(ext_folder,item))
+
+except Exception as ex:
+    print(ex.__name__)     
+
+else :
+    print(f"{os.path.basename(os.path.normpath(folder_path))} {'managed sucessful'}")
